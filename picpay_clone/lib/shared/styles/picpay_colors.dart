9a2a8b253aef1a8c0044fd4760a6cd93b5136949
LilySny <liviacastilholi@gmail.com>
2022-06-16 01:56:55 -0300

    add bottom nav icon and pay button COLORS

--- picpay_clone/lib/shared/styles/picpay_colors.dart
+++ picpay_clone/lib/shared/styles/picpay_colors.dart
@@ -7,8 +7,12 @@ static const PICPAY_CARD_DARK_GREEN = Color.fromRGBO(42, 93, 74, 1);
 
 static const PICPAY_BUTTON_MEDIUM_GREEN = Color.fromRGBO(31, 120, 88, 1);
 
+static const PICPAY_BOTTOM_NAV_BAR_ICON = Color.fromRGBO(0, 101, 51, 1);
+
+static const PICPAY_PAY_BUTTON = Color.fromRGBO(17, 199, 112, 1);
+
 static const PICPAY_WHITE_FONT = Colors.white;
 
-static const PICPAY_GREY_FONT = Colors.grey;
+static const PICPAY_GREY_FONT = Color.fromRGBO(80, 91, 97, 1);
 
 }
